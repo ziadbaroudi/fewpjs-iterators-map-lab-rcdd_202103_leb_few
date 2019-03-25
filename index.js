8c6df9b4732dc98ac587ae0d83036c69cf736c31
Kellye Greene <kellyecodes@gmail.com>
2019-03-25 10:45:15 -0400

    Fixes test

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-function titleCased(tutorials) {
+const tutorials = [
+  'what does the this keyword mean?', 
+  'What is the Contutorialuctor OO pattern?',
+  'implementing Blockchain Web API',
+  'The Test Driven Development Workflow',
+  'What is NaN and how Can we Check for it',
+  'What is the difference between stopPropagation and preventDefault?',
+  'Immutable State and Pure Functions',
+  'what is the difference between == and ===?',
+  'what is the difference between event capturing and bubbling?',
+  'what is JSONP?'
+];
+
+function titleCased() {
   return tutorials.map(function (tutorial) {
     tutorial = tutorial.split(' ');
     for (var i = 0; i < tutorial.length; i++) {
